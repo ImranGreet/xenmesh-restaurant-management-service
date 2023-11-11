e99ea4498443a6905f2528dd43873cc82e1626c1
[Imran Hossain] <88135011+ImranGreet@users.noreply.github.com>
2023-11-12 01:52:10 +0600

    fresh frontend sites are making through vuejs and tailwindcss

--- Frontend/src/composables/date.js
+++
@@ -1,7 +0,0 @@
-let currentDate = new Date();
-
-let currentYear = currentDate.getFullYear();
-let currentMonth = currentDate.getMonth() + 1;
-let currentDay = currentDate.getDate();
-
-export { currentDate, currentYear, currentMonth, currentDay };
--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,14 +1,5 @@
-import { createApp } from 'vue';
-import router from './routes';
-import './assets/css/tailwind.css';
-import VWave from 'v-wave';
-import VueAnimXYZ from '@animxyz/vue3';
-import '@animxyz/core';
-import App from './App.vue';
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
 
-const app = createApp(App);
-app.use(router);
-app.use(VWave);
-app.use(VueAnimXYZ);
-
-app.mount('#app');
+createApp(App).mount('#app')
