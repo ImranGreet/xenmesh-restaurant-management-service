d780da0aad942242fbe89b14a9807ebd54d4904c
[Imran Hossain] <88135011+ImranGreet@users.noreply.github.com>
2023-12-05 21:16:16 +0600

    food items and their category is added

--- Frontend/src/scripts/public/Navs/categoryNav.js
+++ Frontend/src/scripts/public/Navs/categoryNav.js
@@ -1,88 +1,16 @@
 import { ref } from 'vue';
 
-export const useCategoryRoutes = ref([
-  {
-    id: 1,
-    category: 'Appetizers',
-    image:
-      'https://images.unsplash.com/photo-1546069901-ba9599a7e63c?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    description: 'Delicious starters to kick off your meal',
-  },
-  {
-    id: 2,
-    category: 'Mainc',
-    image:
-      'https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?q=80&w=1981&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    description: 'Heartwarming main dishes to satisfy your hunger',
-  },
-  {
-    id: 3,
-    category: 'Desserts',
-    image:
-      'https://images.unsplash.com/photo-1585238342024-78d387f4a707?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    description: 'Sweet treats to end your meal on a high note',
-  },
-  {
-    id: 4,
-    category: 'Beverages',
-    image:
-      'https://images.unsplash.com/photo-1585297099535-d5f84e833797?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    description: 'Refreshing drinks to quench your thirst',
-  },
-  {
-    id: 5,
-    category: 'Salads',
-    image:
-      'https://images.unsplash.com/photo-1585238341710-4d3ff484184d?q=80&w=2004&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    description: 'Healthy and fresh salads for a lighter option',
-  },
-  {
-    id: 6,
-    category: 'Sandwiches',
-    image:
-      'https://images.unsplash.com/photo-1585238342070-61e1e768b1ff?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    description: 'Tasty sandwiches for a quick and satisfying bite',
-  },
-  {
-    id: 7,
-    category: 'Pastasdj',
-    image:
-      'https://images.unsplash.com/photo-1585325701172-fcb7d019242d?q=80&w=1934&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    description: 'Hearty pasta dishes for a comforting experience',
-  },
-  {
-    id: 8,
-    category: 'Speciadsi',
-    image:
-      'https://images.unsplash.com/photo-1512621776951-a57141f2eefd?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    description: 'Unique and flavorful drinks for a special touch',
-  },
-  {
-    id: 9,
-    category: 'Saladsds',
-    image:
-      'https://images.unsplash.com/photo-1543364195-bfe6e4932397?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    description: 'Healthy and fresh salads for a lighter option',
-  },
-  {
-    id: 10,
-    category: 'Sandwiches',
-    image:
-      'https://images.unsplash.com/photo-1503146234394-631200675614?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    description: 'Tasty sandwiches for a quick and satisfying bite',
-  },
-  {
-    id: 11,
-    category: 'Pastadkd',
-    image:
-      'https://images.unsplash.com/photo-1691749123977-0701680083b8?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    description: 'Hearty pasta dishes for a comforting experience',
-  },
-  {
-    id: 12,
-    category: 'Speciausa',
-    image:
-      'https://images.unsplash.com/photo-1690921349852-7694d2b2c447?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    description: 'Unique and flavorful drinks for a special touch',
-  },
-]);
+import items from "../../../DB/products.json";
+
+const uniqueItemCategories = new Set(items.map(item => item.category));
+
+const uniqueCategoriesArray = Array.from(uniqueItemCategories);
+
+const foodCategories = uniqueCategoriesArray.map(category => {
+    return  items.find(item => item.category === category)
+});
+
+
+export const useCategoryRoutes = ref(foodCategories);
+
+
