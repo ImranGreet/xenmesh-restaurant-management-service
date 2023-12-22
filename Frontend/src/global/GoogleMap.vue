e947624924194684cee5d2a30d3e999d425547b1
[Imran Hossain] <88135011+ImranGreet@users.noreply.github.com>
2023-12-22 22:55:19 +0600

    Google map is inited

--- Frontend/src/global/GoogleMap.vue
+++ Frontend/src/global/GoogleMap.vue
@@ -1,34 +1,40 @@
 <template>
   <div
     ref="mapContainer"
-    style="height: 400px"></div>
+    style="height: 500px"></div>
 </template>
 
 <script>
 import { ref, onMounted } from 'vue';
+import useLocationProperties from '../scripts/Googlemap/map';
+const {lat:latitude,long:longitude} = useLocationProperties();
+console.log(latitude);
 
 export default {
   setup() {
     const mapContainer = ref(null);
-    let map; // variable to store the Google Map instance
+    let map; 
+    let currentPosition;
 
     onMounted(() => {
-      // Load the Google Maps JavaScript API
+      /*Load the Google Maps JavaScript API*/
       const script = document.createElement('script');
       script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyCxgFXXZZxArswlgzfwPEC59rNP1cD7N44&callback=initMap`;
       script.defer = true;
       script.async = true;
-      window.initMap = initMap; // Callback function when the script is loaded
+      window.initMap = initMap; 
       document.head.appendChild(script);
     });
 
     function initMap() {
       // Create a new Google Map instance
       map = new google.maps.Map(mapContainer.value, {
-        center: { lat: 37.7749, lng: -122.4194 }, // Initial map center (San Francisco, for example)
-        zoom: 12, // Initial zoom level
+        center: { lat: latitude.value, lng: longitude.value },
+        zoom: 12, 
       });
     }
+
+    
 
     return {
       mapContainer,
